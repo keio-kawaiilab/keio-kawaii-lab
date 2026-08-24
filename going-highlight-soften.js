d6e185bdd2b0793d3683513eeb28236faee53aa8
keio-kawaiilab <keio.kawaiilabfun@gmail.com>
2026-08-25 03:49:26 +0900

    Touch loader after balanced timeline file publish

--- going-highlight-soften.js
+++ going-highlight-soften.js
@@ -29,7 +29,7 @@
   }
   if(!document.querySelector('script[data-ticket-sales-timeline]')){
     var timeline=document.createElement("script");
-    timeline.src="./ticket-sales-timeline-balanced.js?v=202608250349";
+    timeline.src="./ticket-sales-timeline-balanced.js?v=202608250351";
     timeline.setAttribute("data-ticket-sales-timeline","");
     document.body.appendChild(timeline);
   }
