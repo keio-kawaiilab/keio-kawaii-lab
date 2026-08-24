4e98f67435038591be9a9b2e94a219c9f084aaf9
keio-kawaiilab <keio.kawaiilabfun@gmail.com>
2026-08-25 03:48:01 +0900

    Use balanced ticket timeline renderer

--- going-highlight-soften.js
+++ going-highlight-soften.js
@@ -29,7 +29,7 @@
   }
   if(!document.querySelector('script[data-ticket-sales-timeline]')){
     var timeline=document.createElement("script");
-    timeline.src="./ticket-sales-timeline.js?v=202608250342";
+    timeline.src="./ticket-sales-timeline-balanced.js?v=202608250347";
     timeline.setAttribute("data-ticket-sales-timeline","");
     document.body.appendChild(timeline);
   }
