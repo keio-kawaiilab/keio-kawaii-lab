dc13bbde428110c3ae1f6475d1505f99daabde0b
keio-kawaiilab <keio.kawaiilabfun@gmail.com>
2026-08-26 12:04:30 +0900

    chore: bust weather style cache after mobile layout fix

--- schedule-weather.js
+++ schedule-weather.js
@@ -11,7 +11,7 @@
   if(!document.querySelector('link[data-schedule-weather]')){
     var style=document.createElement("link");
     style.rel="stylesheet";
-    style.href="./schedule-weather.css?v=202608260700";
+    style.href="./schedule-weather.css?v=202608261205";
     style.setAttribute("data-schedule-weather","");
     document.head.appendChild(style);
   }
