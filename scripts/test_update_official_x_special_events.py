a5a7555ef24d56c7f8accd007da1add11a355e03
keio-kawaiilab <keio.kawaiilabfun@gmail.com>
2026-08-26 20:02:30 +0900

    test: cover multi-date official X announcements

--- scripts/test_update_official_x_special_events.py
+++ scripts/test_update_official_x_special_events.py
@@ -32,6 +32,21 @@ class OfficialXSpecialEventTests(unittest.TestCase):
         self.assertEqual(event["applicationDisplayMode"], "schedule-only")
         self.assertEqual(event["url"], "https://x.com/SWEET_STEADY/status/2092522442585788839")
 
+    def test_profile_parses_multiple_date_venue_pairs_from_one_post(self):
+        html = '''
+        <html><body>
+        <article itemtype="https://schema.org/SocialMediaPosting" itemid="https://x.com/i/status/222">
+          <meta itemprop="url" content="https://x.com/CANDY_TUNE_/status/222">
+          <meta itemprop="description" content="リリースイベント追加情報\n9/1(火)\n📍埼玉県 エミテラス所沢\n9/14(月)\n📍北海道 サッポロファクトリー アトリウム\n詳細は後日発表">
+        </article>
+        </body></html>
+        '''
+        events = s.parse_profile("CANDY TUNE", "CANDY_TUNE_", html, date(2026, 8, 26))
+        self.assertEqual([(e["eventDate"], e["venue"]) for e in events], [
+            ("2026-09-01", "埼玉県 エミテラス所沢"),
+            ("2026-09-14", "北海道 サッポロファクトリー アトリウム"),
+        ])
+
     def test_profile_ignores_past_special_event(self):
         html = '''
         <article itemtype="https://schema.org/SocialMediaPosting" itemid="https://x.com/i/status/1">
@@ -40,6 +55,10 @@ class OfficialXSpecialEventTests(unittest.TestCase):
         </article>
         '''
         self.assertEqual(s.parse_profile("SWEET STEADY", "SWEET_STEADY", html, date(2026, 8, 26)), [])
+
+    def test_profile_payload_guard_requires_public_timeline(self):
+        self.assertTrue(s.profile_payload_loaded('<article itemtype="SocialMediaPosting"><a href="/status/1">x</a></article>'))
+        self.assertFalse(s.profile_payload_loaded('<html><body>login</body></html>'))
 
     def test_official_site_entry_suppresses_social_placeholder(self):
         payload = {"events": [{
