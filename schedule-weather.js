5be5838ba32a737aac5e818bb8f9abe8b3c6908e
keio-kawaiilab <keio.kawaiilabfun@gmail.com>
2026-08-30 02:28:29 +0900

    Enable GA4 tracking on schedule page

--- schedule-weather.js
+++ schedule-weather.js
@@ -1,6 +1,19 @@
 (function(){
   "use strict";
 
+  if(!window.__KAWAII_GA4_INSTALLED__){
+    window.__KAWAII_GA4_INSTALLED__=true;
+    var measurementId="G-FLWTMG3S7R";
+    window.dataLayer=window.dataLayer||[];
+    window.gtag=window.gtag||function(){window.dataLayer.push(arguments);};
+    window.gtag("js",new Date());
+    window.gtag("config",measurementId);
+    var ga=document.createElement("script");
+    ga.async=true;
+    ga.src="https://www.googletagmanager.com/gtag/js?id="+encodeURIComponent(measurementId);
+    document.head.appendChild(ga);
+  }
+
   function load(src,marker){
     if(document.querySelector('script['+marker+']'))return;
     var script=document.createElement("script");
